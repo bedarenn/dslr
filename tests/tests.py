068d7cee9c1c0788acb971828f5b87cbcc026c8f
Benoit Darenne <bedarenn@3F3.42angouleme.fr>
2025-07-02 15:29:57 +0200

    begin question_1

--- tests/tests.py
+++ tests/tests.py
@@ -1,10 +1,29 @@
-from dslr import read, \
-    str_data
+from dslr import read, get_mean
+
+import matplotlib.pyplot as plt
 
 
 def main():
     df = read("data/dataset_train.csv")
-    print(str_data(df))
+    houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']
+
+    tab = []
+    means = []
+    for i in houses:
+        fam = df[df['Hogwarts House'] == i]
+        tab.append(fam)
+        means.append(get_mean(fam['Astronomy'].dropna().astype(float)))
+
+    plt.figure(figsize=(8, 4.5))
+    plt.grid(axis='y')
+
+    plt.bar(houses, means, color='blue', edgecolor='black')
+
+    plt.title('Astronomy')
+    plt.xlabel('Houses')
+    plt.ylabel('Means')
+
+    plt.show()
 
 
 if __name__ == "__main__":
